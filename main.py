9dae1d4835eb6a995f0560deb391ef61a56d0008
Kaylie Zhao <kaykay@Kaylies-MacBook-Air.local>
2026-03-10 22:27:29 -0700

    syntax

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 import json
 import numpy as np
 
-
-def main():
-    print("Input the dataset you want to use:\n")
-    print("Ex: CS170_Small_DataSet_85")
-
-
-    "On the _th level of the search tree"
-    "--Considering adding the xxx feature"
-    "On level x I added feature x to current set"
-
-
-
-    return
-
-if __name__ == "__main__":
-    main()
-
 size = 0
 
 def accuracy(data, current_set, feature_to_add):
@@ -34,37 +17,33 @@ def accuracy(data, current_set, feature_to_add):
 def feature_search_demo(data):
     curr_set_of_features = [] #initialize an empty set
 
-    for i in size(data, 1) - 1:
+    for i in range(data.shape[1] - 1):
         print("On the ", num2str(i), "th level of the search tree")
         feature_to_add_at_this_level = []
         best_accuracy_so_far = 0
 
         for k in size(data, 1)-1:
-            if isempty(intersect(curr_set_of_features_k))
+            # if isempty(intersect(curr_set_of_features_k))
+            if k not in curr_set_of_features:
+                print("Considering adding the", num2str(k) ,"feature")
+                accuracy = leave_one_out_cross_validation(data, curr_set_of_features, k+1)
 
-            print("Considering adding the", num2str(k) ,"feature")
-            accuracy = leave_one_out_cross_validation(data, curr_set_of_features, k+1)
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_add_at_this_level = k;
 
-            if accuracy > best_accuracy_so_far:
-                best_accuracy_so_far = accuracy
-                feature_to_add_at_this_level = k;
-
-        curr_set_of_features(i) = feature_to_add_at_this_level
+        curr_set_of_features[i] = feature_to_add_at_this_level
         print("On level", num2str(i), "I added feature ", num2str(feature_to_add_at_this_level), " to current set")
 
 # forward search
 #backwards search is same, just change the inequalities
 
 def num2str(index):
+    print("filler for num2str")
     return
 
-#accuracy function
-# def leave_one_out_cross_validation(features):
-#     #echo the numbers it returned to the screen
-#     return 
-
-
 def int2str(i):
+    print("filler for int2str")
     return
 
 #accuracy 
@@ -77,7 +56,7 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
 
     for i in range(data.shape[0]):
         object_to_classify = data[i, 1:]; # to end
-        label_object_to_classify = data(i,1)
+        label_object_to_classify = data[i,1]
 
         nearest_neighbor_distance = float('inf')
         nearest_neighbor_location = float('inf')
@@ -98,6 +77,7 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classified += 1
     accuracy = number_correctly_classified / data.shape[0]
+    return accuracy
 
 
 #early abandoning for omtimization, use global variable inside the fns
@@ -105,3 +85,23 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
 
 def label_object_to_classify():
     return
+
+# main :)
+def main():
+    print("Welcome to the Feature Selection Algorithm\n")
+    print("Input the dataset you want to use:\n")
+    print("Ex: CS170_Small_DataSet_85 \n")
+
+    print("Input the number of the algorithm you want to run\n")
+    print("1) Forward Selection \n")
+    print("2) Backward Selection \n")
+    
+
+    # "On the _th level of the search tree"
+    # "--Considering adding the xxx feature"
+    # "On level x I added feature x to current set"
+
+    return
+
+if __name__ == "__main__":
+    main()
